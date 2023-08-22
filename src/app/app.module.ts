3ca2829090d90be56e9ed4b6e86ecd625034a597
Hossein <amiry.hossein@chmail.ir>
2023-08-22 07:05:12 +0330

    > Add ReactiveFormsModule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BgChangeDirective } from './bg-change.directive';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { BgChangeDirective } from './bg-change.directive';
     BgChangeDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
